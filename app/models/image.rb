a6ef6f41d7f18cc74074346f65765b2233f13f85
agam98 <dwivedi.agam233@gmail.com>
2017-07-04 18:24:25 +0000

    Added image resize functionality

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,15 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  
+  private 
+  
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5 MB")
+    end
+    
+  end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,8 +2,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [300,300]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -36,9 +36,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
