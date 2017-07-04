cc02a18e7dc5e77609765e56b4a77c719b428853
agam98 <dwivedi.agam233@gmail.com>
2017-07-04 18:06:06 +0000

    Added Image Upload functionality

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -25,7 +25,7 @@ class ImagesController < ApplicationController
   # POST /images.json
   def create
     @image = Image.new(image_params)
-
+    @image.user = current_user
     respond_to do |format|
       if @image.save
         format.html { redirect_to @image, notice: 'Image was successfully created.' }
